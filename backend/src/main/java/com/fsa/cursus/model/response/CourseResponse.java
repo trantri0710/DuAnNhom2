7106dad9520b407d5d9fc822aeb61c31e7a25470
Nguyen Nguyen <devncnguyen@gmail.com>
2024-10-27 10:56:07 +0000

    Update CourseResponse.java fix conflict

--- backend/src/main/java/com/fsa/cursus/model/response/CourseResponse.java
+++ backend/src/main/java/com/fsa/cursus/model/response/CourseResponse.java
@@ -1,7 +1,6 @@
 package com.fsa.cursus.model.response;
 
 
-<<<<<<< HEAD
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,15 +9,6 @@ import lombok.Setter;
 public class CourseResponse {
 
     // Khoá Học
-=======
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-public class CourseResponse {
->>>>>>> f262a97ea9e41f16d5fca3b13bb2d211a606eaac
     private Long courseId;
     private String name;
     private String description;
@@ -27,12 +17,7 @@ public class CourseResponse {
     private int totalDuration;
     private double price;
 
-<<<<<<< HEAD
     // Tài Khoản
     private AccountResponse account;
 
-=======
-    private AccountResponse account;
-    private CategoryResponse category;
->>>>>>> f262a97ea9e41f16d5fca3b13bb2d211a606eaac
 }
